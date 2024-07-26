d58051086a8aae38dd20c68379a8a78456b56494
Nitishkumar <nitishkumar.blog@gmail.com>
2024-07-26 09:37:09 +0530

    0.0.0.0 host removed

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ def Static(filename):
     return send_from_directory('Assets', filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host="0.0.0.0")
+    app.run(debug=True, port=5000)
